8101a3669f0fa0868eb55d7cddad806814a4d4cf
Mykhailo Guivan <tornxid123@gmail.com>
2024-04-09 05:11:50 +0200

    registration form small fixes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,53 @@ namespace users_login
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int counter = 0;
+            string login = tbLogin.Text.Trim();
+            string password = pbPassword.Password.Trim();
+            string passwordConfirm = pbConfirmPassword.Password.Trim();
+            string email = tbEmail.Text.Trim().ToLower();
+
+            tbLogin.ToolTip = null;
+            tbLogin.Background = Brushes.Transparent;
+            pbPassword.ToolTip = null;
+            pbPassword.Background = Brushes.Transparent;
+            pbConfirmPassword.ToolTip = null;
+            pbConfirmPassword.Background = Brushes.Transparent;
+            tbEmail.ToolTip = null;
+            tbEmail.Background = Brushes.Transparent;
+
+            if (login.Length < 5)
+            {
+                tbLogin.ToolTip = "Login incorect";
+                tbLogin.Background = Brushes.IndianRed;
+                counter++;
+            } 
+            if (password.Length < 8) 
+            {
+                pbPassword.ToolTip = "Password incorrect";
+                pbPassword.Background = Brushes.IndianRed;
+                counter++;
+            } 
+            if (passwordConfirm != password)
+            {
+                pbConfirmPassword.ToolTip = "Password is not the same";
+                pbConfirmPassword.Background = Brushes.IndianRed;
+                counter++;
+            } 
+            if (email.Length < 8 || !email.Contains('@') || !email.Contains('.'))
+            {
+                tbEmail.ToolTip = "Email incorrect";
+                tbEmail.Background = Brushes.IndianRed;
+                counter++;
+            }
+
+            if (counter == 0)
+            {
+                MessageBox.Show("Done");
+            }
+        }
     }
 }
